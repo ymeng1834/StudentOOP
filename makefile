0894130276c09c1956a3db86f4e1b0c985ed7199
ymeng <ym1834@nyu.edu>
2019-06-04 13:03:27 -0400

    pointers

--- makefile
+++ makefile
@@ -16,9 +16,10 @@ test_pointers: $(TEST_DIR)/test_pointers
 
 $(TEST_DIR)/test_pointers: $(STUDENT_DIR)/pointers.cpp $(TEST_DIR)/test_pointers.cpp
 
-tests: test_hello test_basics
+tests: test_hello test_basics test_pointers
 	tests/test_hello
 	tests/test_basics
+	tests/test_pointers
 
 prod: tests
 	- git commit -a -m "testing new code"
--- my_code/pointers.cpp
+++ my_code/pointers.cpp
@@ -8,11 +8,58 @@ void inc_by_value(int n) {
 }
 
 
-void inc_with_pointer(int* n) {
-    ++(*n);
+void inc_with_pointer(int* iptr) {
+    ++*iptr;
 }
 
 
 void inc_with_reference(int& n) {
     ++n;
 }
+
+
+Thing** create_array_of_things(int n) {
+    Thing** tarray = new Thing* [n];
+    for (int i = 0; i < n; i++) {
+        tarray[i] = new Thing(i);
+    }
+    return tarray;
+}
+
+void print_all_things(Thing** things, int n) {
+    for (int i = 0; i < n; i++) {
+        cout << things[i]->val << " ";
+    }
+    cout << endl;
+}
+
+void double_all_things(Thing** things, int n) {
+    for (int i = 0; i < n; i++) {
+        things[i]->val *= 2;
+    }
+    cout << endl;
+}
+
+void delete_all_things(Thing** things, int n) {
+    for (int i = 0; i < n; i++) {
+        delete things[i];
+    }
+    cout << endl;
+}
+
+void assignTA(Student* s, Student* ta) {
+    s->ta = ta;
+}
+
+void printTAs(vector<Student*>& students) {
+    for (Student* student : students) {
+        if (student->ta == nullptr) {
+            cout << student->name << " has no TA" << endl;
+        }
+        else {
+            cout << student->name << " has TA " << student->ta->name << endl;
+        }
+    }
+}
+
+
